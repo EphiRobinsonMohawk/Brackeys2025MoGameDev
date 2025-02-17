ccee6e845ddfbf04217d3394ee94b1706da08abd
travisdavies-mohawkcollege <travis.davies@mohawkcollege.ca>
2025-02-17 18:04:03 -0500

    Add sprint, Add crouch, Improve movement settings

---
+++ Brackeys2025MoGameDev/Assets/Crouch.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public CharacterController controller;
+    public float crouchSpeed, normalHeight, crouchHeight;
+    public Vector3 offset;
+    public Transform player;
+    bool crouching;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouching = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouching = false;
+        }
+        if (crouching == true)
+        {
+            controller.height = controller.height - crouchSpeed * Time.deltaTime;
+            if (controller.height <= crouchHeight)
+            {
+                controller.height = crouchHeight;
+            }
+            }
+        if (crouching == false)
+        {
+             controller.height = controller.height + crouchSpeed * Time.deltaTime;
+             if (controller.height < normalHeight)
+             {
+                 player.gameObject.SetActive(false);
+                 player.position = player.position + offset * Time.deltaTime;
+                 player.gameObject.SetActive(true);
+             }
+             if (controller.height >= normalHeight)
+             {
+                 controller.height = normalHeight;
+             }
+
+
+            }
+        }
+    }
+
--- Brackeys2025MoGameDev/Assets/PlayerMovement.cs
+++ Brackeys2025MoGameDev/Assets/PlayerMovement.cs
@@ -5,30 +5,118 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
 
-    public float speed = 12f;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public float gravity = -9.81f;
+    public float crouchWalkSpeed;
+    bool crouching = false;
+
+
+    public Vector3 offset;
+
+    public Transform player;
+
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air,
+        crouching
+    }
 
     Vector3 velocity;
+    bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+       // reset velocity when grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        StateHandler();
+        BasicMovement();
+        
+
+
+    }
+
+    private void StateHandler()
+    {
+        //crouching
+        if (Input.GetKeyDown(crouchKey))
+        {
+            crouching = true;
+        }
+        if (Input.GetKeyUp(crouchKey))
+        {
+            crouching = false;
+        }
+        if (crouching == true)
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchWalkSpeed;
+            
+        }
+            
+        
+            
+        
+        
+        //sprinting
+        if (isGrounded && Input.GetKey(sprintKey) && crouching == false)
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        //ground
+        else if (isGrounded && crouching == false)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        else
+        {
+            
+        }
+
+    }
+
+    private void BasicMovement()
+    {
+        // reset velocity when grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        //basic movement and gravity 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * moveSpeed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-
-
     }
+
+
 }
